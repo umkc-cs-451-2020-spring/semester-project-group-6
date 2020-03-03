0a1728926c9c432e5ab76f3453c39de13c468ab0
Linden <las3gk@mail.umkc.edu>
2020-03-03 16:24:57 -0600

    Added Transaction importer

---
+++ CommerceApi/TransactionReader/Transaction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommerceApi
+{
+    public class Transaction
+    {
+        public Transaction(Guid id, string accountNumber, string accountType, string processDate, string balance, string transactionType, string amount, string description)
+        {
+            this.id = id;
+            this.accountNumber = accountNumber;
+            this.accountType = accountType;
+            this.processDate = processDate;
+            this.balance = balance;
+            this.transactionType = transactionType;
+            this.amount = amount;
+            this.description = description;
+        }
+
+        private Guid id { get; set; }
+        private string accountNumber { get; set; }
+        private string accountType { get; set; }
+        private string processDate { get; set; }
+        private string balance { get; set; }
+        private string transactionType { get; set; }
+        private string amount { get; set; }
+        private string description { get; set; }
+
+        // to test output
+        public override string ToString()
+        {
+            return
+                "ID: " + id +
+                " Account Number: " + accountNumber +
+                " Account Type: " + accountType +
+                " Processing Date: " + processDate +
+                " Balance: " + balance +
+                " Transaction Type: " + transactionType +
+                " Amount: " + amount +
+                " Description: " + description;
+        }
+
+
+
+    }
+}
---
+++ CommerceApi/TransactionReader/TransactionReader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CommerceApi
+{
+    public class TransactionReader
+    {
+        public static List<Transaction> ImportTransactions(string file)
+        {
+            List<string> lines = new List<string>();
+            List<Transaction> transactionList = new List<Transaction>();
+
+            try
+            {
+                lines = File.ReadAllLines(file).ToList();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e);
+            }
+           
+            foreach (string line in lines)
+            {
+                string[] cell = line.Split(',');
+                Transaction transaction = new Transaction(
+                    Guid.NewGuid(),
+                    cell[0],
+                    cell[1],
+                    cell[2],
+                    cell[3],
+                    cell[4],
+                    cell[5],
+                    cell[6]);
+                transactionList.Add(transaction);
+            }
+
+            // print to console for testing
+            /*
+            foreach (Transaction transaction in transactionList)
+            {
+                Console.WriteLine(transaction);
+            }
+            // Console.ReadLine();
+            */ 
+
+            return transactionList;
+        }
+    }
+}
